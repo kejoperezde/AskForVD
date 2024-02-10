6fa80e773851776a763235427b8f1c215fbc9b70
kejoperezde <kejoperezde@ottepic.edu.mx>
2024-02-09 19:20:15 -0600

    Prueba 1

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ function App() {
       {
         !valueSi ? (
           <div className="p-5">
-            <h1 className="text-white font-bold text-5xl text-center">¿Quieres ser mi San Valentin?</h1>
+            <h1 className="text-white font-bold text-5xl text-center">Stephanny❤️, ¿Quieres ser mi San Valentin?</h1>
             <img src={Object.keys(randomValor).length === 0 ?
               "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif" : randomValor.img} alt="San Valentin" className="mx-auto" width={400} height={400} />
             <div className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-5 items-center">
@@ -97,7 +97,7 @@ function App() {
                 setValueSi(true)
 
                 jsConfetti.addConfetti({
-                  emojis: ['😍', '🥰', '❤️', '😘'],
+                  emojis: ['😍', '🥰', '❤️', '😘', '🦷'],
                   emojiSize: 70,
                   confettiNumber: 80,
                 })
@@ -117,7 +117,7 @@ function App() {
           </div>
         ) : (
           <div className="flex justify-center items-center flex-col space-y-10">
-            <h1 className="text-4xl text-white font-bold">Sabia que dirias que si ❤️!</h1>
+            <h1 className="text-4xl text-white font-bold">Sabía que dirias que si❤️!</h1>
             <img src="https://i.pinimg.com/originals/9b/dc/c6/9bdcc6206c1d36a37149d31108c6bb41.gif" alt="" className="mx-auto" />
             <span hidden>{document.title = 'Sabia que dirias que si ❤️!'}</span>
           </div>
